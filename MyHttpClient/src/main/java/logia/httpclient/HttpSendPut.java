d2662e4a39d56994c71e7648f02b5fd54575c854
Paul Mai <dai.maithanh@gmail.com>
2016-06-14 18:16:39 +0700

    Fix bug utf8 of http request body

--- MyHttpClient/src/main/java/logia/httpclient/HttpSendPut.java
+++ MyHttpClient/src/main/java/logia/httpclient/HttpSendPut.java
@@ -1,7 +1,6 @@
 package logia.httpclient;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -10,14 +9,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import logia.httpclient.response.listener.HttpResponseListener;
-
+import org.apache.commons.io.Charsets;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.message.BasicNameValuePair;
+
+import logia.httpclient.response.listener.HttpResponseListener;
 
 /**
  * The Class HttpSendPut.
@@ -26,67 +26,67 @@ import org.apache.http.message.BasicNameValuePair;
  */
 public class HttpSendPut extends HttpUtility {
 
-	/**
-	 * Instantiates a new http send put.
-	 *
-	 * @param __host the host
-	 * @param __requestURL the request url
-	 * @param __headers the headers
-	 * @param __parameters the params
-	 * @param __listener the __listener
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 * @throws KeyManagementException the key management exception
-	 * @throws NoSuchAlgorithmException the no such algorithm exception
-	 * @throws KeyStoreException the key store exception
-	 */
-	public HttpSendPut(HttpHost __host, String __requestURL, Map<String, String> __headers, Map<String, String> __parameters,
-	        HttpResponseListener<?> __listener) throws IOException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		super(__host, __requestURL, __headers, __parameters, __listener);
-		this.httpRequest = new HttpPut(this.requestURL);
-		this.setHeaders();
-		this.setParameters();
-	}
+    /**
+     * Instantiates a new http send put.
+     *
+     * @param __host the host
+     * @param __requestURL the request url
+     * @param __headers the headers
+     * @param __parameters the params
+     * @param __listener the __listener
+     * @throws IOException Signals that an I/O exception has occurred.
+     * @throws KeyManagementException the key management exception
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     * @throws KeyStoreException the key store exception
+     */
+    public HttpSendPut(HttpHost __host, String __requestURL, Map<String, String> __headers,
+            Map<String, String> __parameters, HttpResponseListener<?> __listener)
+            throws IOException, KeyManagementException, NoSuchAlgorithmException,
+            KeyStoreException {
+        super(__host, __requestURL, __headers, __parameters, __listener);
+        this.httpRequest = new HttpPut(this.requestURL);
+        this.setHeaders();
+        this.setParameters();
+    }
 
-	/**
-	 * Instantiates a new http send put.
-	 *
-	 * @param __requestURL the request url
-	 * @param __headers the headers
-	 * @param __parameters the params
-	 * @param __listener the __listener
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 * @throws KeyManagementException the key management exception
-	 * @throws NoSuchAlgorithmException the no such algorithm exception
-	 * @throws KeyStoreException the key store exception
-	 */
-	public HttpSendPut(String __requestURL, Map<String, String> __headers, Map<String, String> __parameters, HttpResponseListener<?> __listener)
-	        throws IOException, KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
-		super(__requestURL, __headers, __parameters, __listener);
-		this.httpRequest = new HttpPut(this.requestURL);
-		this.setHeaders();
-		this.setParameters();
-	}
+    /**
+     * Instantiates a new http send put.
+     *
+     * @param __requestURL the request url
+     * @param __headers the headers
+     * @param __parameters the params
+     * @param __listener the __listener
+     * @throws IOException Signals that an I/O exception has occurred.
+     * @throws KeyManagementException the key management exception
+     * @throws NoSuchAlgorithmException the no such algorithm exception
+     * @throws KeyStoreException the key store exception
+     */
+    public HttpSendPut(String __requestURL, Map<String, String> __headers,
+            Map<String, String> __parameters, HttpResponseListener<?> __listener)
+            throws IOException, KeyManagementException, NoSuchAlgorithmException,
+            KeyStoreException {
+        super(__requestURL, __headers, __parameters, __listener);
+        this.httpRequest = new HttpPut(this.requestURL);
+        this.setHeaders();
+        this.setParameters();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see logia.utility.httpclient.HttpUtility#setParameters()
-	 */
-	@Override
-	protected void setParameters() {
-		if (this.params == null) {
-			return;
-		}
-		List<NameValuePair> _urlParameters = new ArrayList<NameValuePair>();
-		for (Entry<String, String> _param : this.params.entrySet()) {
-			_urlParameters.add(new BasicNameValuePair(_param.getKey(), _param.getValue()));
-		}
-		try {
-			((HttpEntityEnclosingRequest) this.httpRequest).setEntity(new UrlEncodedFormEntity(_urlParameters));
-		}
-		catch (UnsupportedEncodingException e) {
-			HttpUtility.LOGGER.error(e);
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see logia.utility.httpclient.HttpUtility#setParameters()
+     */
+    @Override
+    protected void setParameters() {
+        if (this.params == null) {
+            return;
+        }
+        List<NameValuePair> _urlParameters = new ArrayList<NameValuePair>();
+        for (Entry<String, String> _param : this.params.entrySet()) {
+            _urlParameters.add(new BasicNameValuePair(_param.getKey(), _param.getValue()));
+        }
+        ((HttpEntityEnclosingRequest) this.httpRequest)
+                .setEntity(new UrlEncodedFormEntity(_urlParameters, Charsets.UTF_8));
+    }
 
 }
